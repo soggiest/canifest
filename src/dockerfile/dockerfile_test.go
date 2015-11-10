5a5e3a14a79918164302f464c947a6df9b6023e4
Øystein Bedin <obedin@redhat.com>
2015-11-10 11:29:52 -0700

    Updated to not use != logic

--- src/dockerfile/dockerfile_test.go
+++ src/dockerfile/dockerfile_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestExists(t *testing.T) {
-	if exists("/etc") != true {
+	if exists("/etc") == false {
 		// t.Errorf("File exists, but function returned false...")
 		t.Fatalf("File exists, but function returned false...")
 	}	
